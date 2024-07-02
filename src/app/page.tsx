ea97b42e4f0f944b8204f7b91175a97a235a9bf3
Salavat Faizullin <s.faizullin@innopolis.university>
2024-07-02 18:28:45 +0300

    Added Next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import './globals.css'
+export default function Home() {
+  return <div className='catalog'>
+    <ul className='product_list'>
+      <li className='product_card_container'>
+        <div className='product_card'>
+          <div className="image-cont" data-v-2d064667="">
+            <img className="product_img" src="https://media.5ka.ru/media/products/3698647.jpg" alt="Колбаса Мясницкий Ряд Для завтрака вареная 400г" data-v-2d064667="" />
+            <div className="discount-hint" data-v-2d064667="">
+              -37 %
+            </div></div>
+          <div className="prices" data-v-2d064667=""><div className="price-discount" data-v-2d064667=""><span data-v-2d064667=""><span className="from" data-v-2d064667="">от</span>
+            139
+          </span> <div className="price-right" data-v-2d064667=""><span className="price-discount_cents" data-v-2d064667="">
+            99
+          </span> <span className="price-regular" data-v-2d064667="">
+                219
+                <sup data-v-2d064667="">.99</sup></span></div></div></div>
+          <div className="item-name" data-v-2d064667="">
+            Колбаса Мясницкий Ряд Для...
+          </div>
+          <div className="item-date" data-v-2d064667=""><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" data-v-2d064667=""><path fill-rule="evenodd" clip-rule="evenodd" d="M8.85 1C8.38056 1 8 1.38056 8 1.85C8 2.31944 8.38056 2.7 8.85 2.7H15.15C15.6194 2.7 16 2.31944 16 1.85C16 1.38056 15.6194 1 15.15 1H8.85ZM12 21C16.4663 21 20 17.4828 20 13.25C20 9.01722 16.4663 5.5 12 5.5C7.53369 5.5 4 9.01722 4 13.25C4 17.4828 7.53369 21 12 21ZM12 23C17.5228 23 22 18.6348 22 13.25C22 7.86522 17.5228 3.5 12 3.5C6.47715 3.5 2 7.86522 2 13.25C2 18.6348 6.47715 23 12 23ZM11.0044 13.7369C11.0479 14.1655 11.4099 14.5 11.85 14.5H16.15C16.6194 14.5 17 14.1194 17 13.65C17 13.1806 16.6194 12.8 16.15 12.8H12.7V9.35C12.7 8.88056 12.3194 8.5 11.85 8.5C11.3806 8.5 11 8.88056 11 9.35V13.65C11 13.6793 11.0015 13.7083 11.0044 13.7369Z" fill="#b2b2b2"></path></svg>
+            до 17 июля
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+}
