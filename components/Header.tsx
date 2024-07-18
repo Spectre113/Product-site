60921a014cad7fd60927916c6df7868ffa91b615
Владимир Топорков <VTvolody626@mail.ru>
2024-07-19 00:36:19 +0700

    register form added

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,6 +8,22 @@ import { useRouter } from 'next/router';
 const Header: React.FC = () => {
     const router = useRouter();
     useEffect(() => {
+        const basketBtn = document.querySelector('.header__basket');
+        const basketBlock = document.querySelector('.header__basket-block');
+
+        if (basketBtn && basketBlock) {
+            basketBtn.addEventListener('click', function(event) {
+                event.stopPropagation();
+                basketBlock.classList.remove('none');
+            });
+
+            document.addEventListener('click', function (event) {
+                if (!basketBlock.contains(event.target as Node)) {
+                    basketBlock.classList.add('none');
+                }
+            });
+        }
+
         const search = document.querySelector('.header__search--disabled') as HTMLElement | null;
         const nav = document.querySelector('.header__nav') as HTMLElement | null;
         const searchContent = document.querySelector('.header__search-active') as HTMLElement | null;
@@ -48,8 +64,12 @@ const Header: React.FC = () => {
         const logBtn = document.querySelector('.header__log');
         const logContent = document.querySelector('.header__log-content');
         const logClose = document.querySelector('.header__log-close');
+        const registerBtn = document.querySelector('.header__log-register');
+        const registerBlock = document.querySelector('.header__register-block');
+        const registerClose = document.querySelector('.register-close');
+        const registerSuccessMessage = document.querySelector('.register--successful');
 
-        if (logBtn && logContent && logClose) {
+        if (logBtn && logContent && logClose && registerBtn && registerBlock && registerClose && registerSuccessMessage) {
             logBtn.addEventListener('click', function(event){
                 event.stopPropagation();
                 logContent.classList.remove('none');
@@ -58,6 +78,18 @@ const Header: React.FC = () => {
             logClose.addEventListener('click', function(event) {
                 event.stopPropagation();
                 logContent.classList.add('none');
+                registerSuccessMessage.classList.add('none');
+            });
+
+            registerBtn.addEventListener('click', function(event) {
+                event.stopPropagation();
+                logContent.classList.add('none');
+                registerBlock.classList.remove('none');
+            });
+
+            registerClose.addEventListener('click', function(event) {
+                event.stopPropagation();
+                registerBlock.classList.add('none');
             });
         }
 
@@ -105,6 +137,45 @@ const Header: React.FC = () => {
               router.push('/apanel');
             } else {
               alert('Invalid login or password');
+            }
+          });
+
+          const validator3 = new JustValidate('#log-form-3');
+
+        validator3
+        .addField('#name', [
+            {
+            rule: 'required',
+            errorMessage: 'You did not enter a name',
+            },
+            {
+            rule: 'minLength',
+            value: 3,
+            errorMessage: '3 symbols minimum',
+            },
+            {
+            rule: 'maxLength',
+            value: 30,
+            errorMessage: '30 symbols maximum',
+            },
+        ])
+        .addField('#password', [
+            {
+            rule: 'required',
+            errorMessage: 'You did not enter a password',
+            },
+            {
+            rule: 'minLength',
+            value: 3,
+            errorMessage: '3 symbols minimum',
+            },
+        ])
+        .onSuccess((event : Event) => {
+            event.stopPropagation();
+            if (registerBlock && logContent && registerSuccessMessage) {
+                registerBlock.classList.add('none');
+                logContent.classList.remove('none');
+                registerSuccessMessage.classList.remove('none');
             }
           });
       }, [router]);
@@ -188,11 +259,14 @@ const Header: React.FC = () => {
                                     <path d="M29.6777 12L12 29.6777M29.6777 29.6777L12 12" stroke="#A1A6B4" strokeWidth="3"></path>
                                 </svg>
                             </button>
+                            <p className="header__log-info register--successful none">
+                                You have successfully registered, now you may log-in:
+                            </p>
                             <h2 className="header__log-title">
                                 Log-in
                             </h2>
                             <p className="header__log-info">
-                                If you don't have an account yet, <a href="#">register here</a>.
+                                If you don't have an account yet, <button className='btn-reset header__log-register'>register here</button>.
                             </p>
                             <form action="https://jsonplaceholder.typicode.com/posts" method="POST" className="header__log-form flex" id="log-form">
                                 <label htmlFor="name" className="header__log-label">
@@ -207,12 +281,36 @@ const Header: React.FC = () => {
                             </form>
                         </div>
                     </div>
+                    <div className="header__register-block none flex">
+                        <div>
+                            <button className="register-close header__log-close btn-reset">
+                                <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <circle cx="21" cy="21" r="19.5" stroke="#A1A6B4" strokeWidth="3"></circle>
+                                    <path d="M29.6777 12L12 29.6777M29.6777 29.6777L12 12" stroke="#A1A6B4" strokeWidth="3"></path>
+                                </svg>
+                            </button>
+                            <h2 className="header__log-title">
+                                Register
+                            </h2>
+                            <form action="https://jsonplaceholder.typicode.com/posts" method="POST" className="header__log-form flex" id="log-form-3">
+                                <label htmlFor="name" className="header__log-label">
+                                    <input type="text" className="header__log-input" placeholder="Enter a name" name="name" id="name"></input>
+                                </label>
+                                <label htmlFor="password" className="header__log-label">
+                                    <input type="password" className="header__log-input" placeholder="Enter your password" name="password" id="password"></input>
+                                </label>
+                                <button className="header__log-button btn-reset">
+                                    Register
+                                </button>
+                            </form>
+                        </div>
+                    </div>
                     <button className="header__basket btn-reset">
                         <svg fill="#fff" width="32px" height="30px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
                             <path d="M31.739 8.875c-0.186-0.264-0.489-0.422-0.812-0.422h-21.223l-1.607-5.54c-0.63-2.182-2.127-2.417-2.741-2.417h-4.284c-0.549 0-0.993 0.445-0.993 0.993s0.445 0.993 0.993 0.993h4.283c0.136 0 0.549 0 0.831 0.974l5.527 20.311c0.12 0.428 0.511 0.724 0.956 0.724h13.499c0.419 0 0.793-0.262 0.934-0.657l4.758-14.053c0.11-0.304 0.064-0.643-0.122-0.907zM25.47 22.506h-12.046l-3.161-12.066h19.253zM23.5 26.504c-1.381 0-2.5 1.119-2.5 2.5s1.119 2.5 2.5 2.5 2.5-1.119 2.5-2.5c0-1.381-1.119-2.5-2.5-2.5zM14.5 26.504c-1.381 0-2.5 1.119-2.5 2.5s1.119 2.5 2.5 2.5 2.5-1.119 2.5-2.5c0-1.381-1.119-2.5-2.5-2.5z"></path>
                         </svg>
                     </button>
-                    <div className="header__basket-block">
+                    <div className="header__basket-block none">
                         <div className="basket-container">
                             {/* Вот тут вот название товара и прайс товара и кнопка удалить */}
                         </div>
