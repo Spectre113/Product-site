b34c3f7bc9e635f1a7bea307affb75ab1d91cdba
Владимир Топорков <VTvolody626@mail.ru>
2024-07-22 05:23:21 +0700

    bugs fixed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: true,
-    images: {
-      unoptimized: true,
-    },
-    output: 'export',
-    distDir: 'out'
-  };
-  
-  module.exports = nextConfig;
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  output: 'export',
+  distDir: 'out',
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    const { '/api/create': removed, ...pathMapWithoutApiCreate } = defaultPathMap;
+    return pathMapWithoutApiCreate;
+  },
+};
+
+module.exports = nextConfig;
